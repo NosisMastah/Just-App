d6ada595979b3fe49021d0133b50916c2030f379
Diogo Roque <diogorq2@gmail.com>
2024-10-10 20:53:48 +0100

    Adicionado suporte com o firebase para Android, IOS e Web

--- just_app/lib/main.dart
+++ just_app/lib/main.dart
@@ -1,9 +1,27 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'src/settings/settings_controller.dart';
 import 'src/settings/settings_service.dart';
 import 'src/screens/login/login.dart'; // Importando a página de login
+import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
+	WidgetsFlutterBinding.ensureInitialized();
+
+  if(kIsWeb){
+		await Firebase.initializeApp(
+			options: FirebaseOptions(
+				apiKey: "AIzaSyCJiz9OW-IRp93L8qUWZvYesr33nA6lBL8",
+				authDomain: "just-app-358a0.firebaseapp.com",
+				projectId: "just-app-358a0",
+				storageBucket: "just-app-358a0.appspot.com",
+				messagingSenderId: "1041744619832",
+				appId: "1:1041744619832:web:12a9ef980354e6c5fcf9cd",
+				measurementId: "G-6F4RKL45H2"));
+	}else{
+		await Firebase.initializeApp();
+	}
+
   final settingsController = SettingsController(SettingsService());
   await settingsController.loadSettings();
   runApp(MyApp(settingsController: settingsController));
